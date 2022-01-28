310a2aad671b282fad34ffde4c058adf57732b94
ignatif <ignatif@gmail.com>
2022-01-28 16:08:46 +0200

    fix typo in code

--- src/App.js
+++ src/App.js
@@ -353,7 +353,7 @@ const Icon = ({
     onClick={onClick}
     data-tip
     data-for={dataFor}
-    stlye={{ minWidth: '1.25rem' }}
+    style={{ minWidth: '1.25rem' }}
   >
     {children}
   </div>
